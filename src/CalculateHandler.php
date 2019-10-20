849f6a85c0e21f77cb75783d0355521cf99ff054
Vladimir Shelestov aka DarthShelL <shelestov@techcd.ru>
2019-10-20 23:43:45 +0300

    #major!

--- src/CalculateHandler.php
+++ src/CalculateHandler.php
@@ -23,9 +23,44 @@ class CalculateHandler implements CalculateInterface
 
     private function checkInput(string $input): bool
     {
+        if (!is_string($input)) {
+            throw new \ErrorException(sprintf(
+                'Incorrect input data type! Input data must be a string.',
+                $input
+            ));
+        }
+
         if (!preg_match('/^([0-9-+*\/ ]|(\d[.]\d))+$/', $input)) {
             throw new \ErrorException(sprintf(
                 'Incorrect input data "%s". Try to use numbers and simple operators e.g. [ + - / * ]',
+                $input
+            ));
+        }
+
+        if (preg_match('/(\/\s?[0])/', $input)) {
+            throw new \ErrorException(sprintf(
+                'Division by Zero!! Are u kidding with me?',
+                $input
+            ));
+        }
+
+        if (preg_match('/([+-\/*])$/', $input)) {
+            throw new \ErrorException(sprintf(
+                'Is anything missing?',
+                $input
+            ));
+        }
+
+        if (preg_match('/^(\s?[+\/*])/', $input)) {
+            throw new \ErrorException(sprintf(
+                'May be you\'ll try to start with number?',
+                $input
+            ));
+        }
+
+        if (preg_match('/(\d\s+\d)/', $input)) {
+            throw new \ErrorException(sprintf(
+                'Is anything missing?',
                 $input
             ));
         }
---
+++ tests/CalculateFromStringTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace darthshell\cfs\Tests;
+
+use darthshell\cfs\CalculateFromString;
+use PHPUnit\Framework\TestCase;
+
+class CalculateFromStringTest extends TestCase
+{
+    protected $fixture;
+
+    protected function setUp()
+    {
+        $this->fixture = new CalculateFromString();
+    }
+
+    protected function tearDown()
+    {
+        $this->fixture = NULL;
+    }
+
+    /**
+     * @dataProvider provider4Exception
+     */
+    public function testInputCheck($input)
+    {
+        $this->expectException(\ErrorException::class);
+        $this->fixture->calculate($input);
+    }
+
+    public function testReturnedType()
+    {
+        $result = $this->fixture->calculate('2+2*2');
+        $this->assertTrue(is_string($result), "Got a " . gettype($result) . " instead of a string");
+    }
+
+    /**
+     * @dataProvider provider4Calculation
+     */
+    public function testCalculation($input, $result)
+    {
+        $this->assertEquals($result, $this->fixture->calculate($input));
+    }
+
+    public function provider4Calculation()
+    {
+        return [
+            ['2+2*2', '6'],
+            ['2+2/2', '3'],
+            ['2+2*10', '22'],
+            ['23/2', '11.5'],
+            ['2/32*5', '0.3125']
+        ];
+    }
+
+    public function provider4Exception()
+    {
+        return [
+            ['250%-20'],
+            ['abc'],
+            ['20/0'],
+            ['20/ 0'],
+            ['5+.3'],
+            ['* 23 - 345'],
+            ['34/67/'],
+            ['34 / 6 7 /'],
+        ];
+    }
+}
